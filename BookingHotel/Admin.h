8e04a1ab80b94f500d4ba7e3f9fed0e6fea75594
Unknown <Nutp1999@gmail.com>
2018-11-14 23:01:16 +0700

    Update Admin.h

--- BookingHotel/Admin.h
+++ BookingHotel/Admin.h
@@ -1,8 +1,19 @@
 //
 // Created by NP-1999 on 11/14/2018.
 //
-
 #ifndef BOOKINGHOTEL_ADMIN_H
 #define BOOKINGHOTEL_ADMIN_H
+
+#include<stdio.h>
+#include<string.h>
+
+void login(char* user,char* pass){
+    int check_user = strcmp(user,"Admin"),
+        check_pass = strcmp(pass,"rootAdmin");
+    if(!(check_user && check_pass)){
+        printf("Username or Password is invalid");
+        return;
+    }
+}
 
 #endif //BOOKINGHOTEL_ADMIN_H
